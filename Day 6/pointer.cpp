d7743651e9de53a0ff60ffc558592cc6c997676b
Sanjana Bansal <sbansal9347@gmail.com>
2026-05-07 10:56:10 +0530

    day 6

--- Day 6/pointer.cpp
+++ Day 6/pointer.cpp
@@ -1,1 +1,11 @@
-hgkujk
+#include<iostream>
+using namespace std;
+int main(){
+    int a = 10;
+    int *ptr = &a;
+    cout<<a;
+    cout<<*ptr;
+    cout<<ptr;
+    cout<<&a;
+    cout<<&ptr;
+}
--- Day 6/pointer2.cpp
+++ Day 6/pointer2.cpp
@@ -1,1 +1,14 @@
-j
+#include<iostream>
+using namespace std;
+int main(){
+    int a = 2;
+    int *ptr = &a;
+    int **ptr2 = &ptr;
+    cout<<a;
+    cout<<*ptr;
+    cout<<ptr;
+    cout<<&a;
+    cout<<&ptr;
+    cout<<*ptr2;
+    cout<<ptr2;
+}
