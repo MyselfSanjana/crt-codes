6ad60285811be37acb27be69bd78d65e05fcaf97
Sanjana Bansal <sbansal9347@gmail.com>
2026-05-09 09:30:20 +0530

    day9

---
+++ Day 9/Inheritence.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+ //inheritence
+
+ class Human{
+    //properties
+    protected:
+    string name;
+
+ };
+ class Student : public Human{
+    int rollNo;
+    public:
+
+    Student(string n , int a){
+        name = n;
+        rollNo = a;
+    }
+    void display(){
+        cout<<name<<endl<<rollNo<<endl;
+    }
+ };
+ int main(){
+    Student obj("sanjana", 123);
+    obj.display();
+    return 0;
+ }
