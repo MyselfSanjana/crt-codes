e10d048c8a3ae0aab1446f731f357a857d9e2b44
Sanjana Bansal <sbansal9347@gmail.com>
2026-05-13 12:09:38 +0530

    recursion

--- Day 13/ReverseRecursion.cpp
+++ Day 13/ReverseRecursion.cpp
@@ -1,18 +1,18 @@
 #include <iostream>
 using namespace std;
 
-void reverseArray(int arr[],int n, int i , int j){
-    while(i<j){
-        swap(arr[i],arr[j]);
-        i++;
-        j--;
+void reverseArray(int arr[],int i , int j){
+    if(i>j){
+        return;
     }
+        swap(arr[i],arr[j]);
+        reverseArray(arr,i+1,j-1);
 }
 
 int main(){
     int arr[6] = {1,2,3,4,5,6};
     int n = 6;
-    reverseArray(arr,n,0,n-1);
+    reverseArray(arr,0,n-1);
     for(int i = 0; i<n; i++){
         cout<<arr[i]<<" ";
     }
